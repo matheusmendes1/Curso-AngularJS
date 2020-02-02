8ab665fc3e1678b5d3cadaf8cb082dd66a27bad0
Matheus Santos <matheusmendes1@hotmail.com>
2020-02-02 17:22:19 -0300

    End Chapter 2

--- public/js/controllers/fotos-controller.js
+++ public/js/controllers/fotos-controller.js
@@ -2,11 +2,12 @@
 /* Angular disponibiliza uma ponte de ligação entre o controller e a view chamada $scope e tudo que for "pendurado" neste objeto será enxergado pela view */
 angular
     .module('alurapic')
-    .controller('FotosController', function($scope) {
+    .controller('FotosController', function($scope, $http) {
 
-    $scope.foto = {
-        titulo : 'Leão',
-        url : 'http://www.fundosanimais.com/Minis/leoes.jpg'
-    };
+    $scope.fotos = [];
 
+    $http
+        .get('v1/fotos')
+        .success((fotos) => $scope.fotos = fotos)
+        .error((error) => console.log(error));
 });
